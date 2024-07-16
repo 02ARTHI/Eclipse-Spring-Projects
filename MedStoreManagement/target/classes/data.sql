a11ad470e63f2bb6668440dcf762fb077d7bb062
Arthi Selavarj <164236172+02ARTHI@users.noreply.github.com>
2024-07-16 23:37:49 +0530

    Add files via upload

---
+++ MedStoreManagement/target/classes/data.sql
@@ -0,0 +1,14 @@
+insert into medi_store(ID, USERNAME, DESCRIPTION,TARGET_DATE,DONE)
+values(10001,'ahappysoul','learn AWS',CURRENT_DATE(),true);
+
+insert into medi_store(ID, USERNAME, DESCRIPTION,TARGET_DATE,DONE)
+values(10002,'ahappysoul','learn devops',CURRENT_DATE(),true);
+
+insert into medi_store(ID, USERNAME, DESCRIPTION,TARGET_DATE,DONE)
+values(10003,'ahappysoul','learn backend',CURRENT_DATE(),true);
+
+insert into medi_store(ID, USERNAME, DESCRIPTION,TARGET_DATE,DONE)
+values(10004,'ahappysoul','learn frontend',CURRENT_DATE(),true);
+
+insert into medi_store(ID, USERNAME, DESCRIPTION,TARGET_DATE,DONE)
+values(10005,'ahappysoul','learn data science',CURRENT_DATE(),true);
