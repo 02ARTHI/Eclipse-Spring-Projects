6dde0c8ae3d21d705c6e82b8cc5a92082d0b1d1f
Arthi Selvaraj <164236172+02ARTHI@users.noreply.github.com>
2024-07-24 23:33:00 +0530

    Add files via upload

---
+++ springBootEC2Web/src/main/java/com/learning/springBootEC2Web/SpringBootEc2WebApplication.java
@@ -0,0 +1,13 @@
+package com.learning.springBootEC2Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEc2WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEc2WebApplication.class, args);
+	}
+
+}
---
+++ springBootEC2Web/src/test/java/com/learning/springBootEC2Web/SpringBootEc2WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springBootEC2Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEc2WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
